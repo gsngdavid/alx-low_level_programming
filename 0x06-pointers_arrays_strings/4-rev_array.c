f02de1d96aa968ceb45ed0dc58a02ddaebab11b5
mbukeRepo <mbuke404@gmail.com>
2022-06-16 15:11:15 +0200

    reversing an array

---
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -0,0 +1,15 @@
+/**
+* reverse_array - it will reverse the array
+* @a: the array of elements
+* @n: the size of an array
+*/
+void reverse_array(int *a, int n)
+{
+int i;
+for (i = 0; i <= n / 2; i++)
+{
+int temp = a[i];
+a[i] = a[n - 1 - i];
+a[n - 1 - i] = temp;
+}
+}
