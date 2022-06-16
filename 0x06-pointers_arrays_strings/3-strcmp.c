08e41a75232d3b86fa489311d3361e2a0cf03453
mbukeRepo <mbuke404@gmail.com>
2022-06-16 14:42:58 +0200

    3-strcmp.c

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,12 @@
+#include<string.h>
+/**
+* _strcmp - compares s1 and s2
+* @s1: first string
+* @s2: secong string
+* Return: returns an integer when s1 is found
+*/
+
+int _strcmp(char *s1, char *s2)
+{
+return (strcmp(s1, s2));
+}
