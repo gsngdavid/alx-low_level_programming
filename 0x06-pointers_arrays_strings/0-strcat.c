f6fc8700a5f9c458af99879f83909ce14ad58798
mbukeRepo <mbuke404@gmail.com>
2022-06-16 13:35:18 +0200

    0. strcat

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,12 @@
+#include<string.h>
+
+/**
+*  _strcat - for concatinating strings
+*  @dest: wher to copy to
+*  @src: where to copy from
+*  Return: destination string
+*/
+char *_strcat(char *dest, char *src)
+{
+return (strcat(dest, src));
+}
